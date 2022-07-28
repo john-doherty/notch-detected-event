92410977a93f7c9662444d6d7f1edf4103a71917
John Doherty <john@orcascan.com>
2022-07-28 18:04:58 +0200

    Fixed #2

--- src/notch-detected-event.js
+++ src/notch-detected-event.js
@@ -10,8 +10,54 @@
 
     'use strict';
 
-    // keep a pointer to the HTML element for speed
-    var root = null;
+    // only execute this once the document has loaded (this might need to change!)
+    window.addEventListener('load', function() {
+
+        // we need to wait before executing to allow layout to happen
+        setTimeout(function() {
+
+            // get the <html> element
+            var root = document.documentElement;
+
+            // add CSS variables so we can read them back
+            root.style.setProperty('--notch-top', 'env(safe-area-inset-top)');
+            root.style.setProperty('--notch-right', 'env(safe-area-inset-right)');
+            root.style.setProperty('--notch-bottom', 'env(safe-area-inset-bottom)');
+            root.style.setProperty('--notch-left', 'env(safe-area-inset-left)');
+
+            // get runtime styles
+            var computedStyle = window.getComputedStyle(root);
+
+            // read env values back and check if we have any values
+            var hasNotch = [
+                computedStyle.getPropertyValue('--notch-top') || '-1',
+                computedStyle.getPropertyValue('--notch-right') || '-1',
+                computedStyle.getPropertyValue('--notch-bottom') || '-1',
+                computedStyle.getPropertyValue('--notch-left') || '-1'
+            ]
+            .map(parseInt)
+            .filter(function(val) {
+                return val > 0;
+            })
+            .length > 0;
+
+            // only if we have a notch
+            if (hasNotch) {
+
+                // remove CSS variables
+                root.style.removeProperty('--notch-top');
+                root.style.removeProperty('--notch-right');
+                root.style.removeProperty('--notch-bottom');
+                root.style.removeProperty('--notch-left');
+
+                // set <html data-notch="true"> to allow CSS to tweak the display
+                root.setAttribute('data-notch', 'true');
+
+                // fire global notch-detected event to let the UI know that happened
+                window.dispatchEvent(new CustomEvent('notch-detected', { bubbles: true, cancelable: true }));
+            }
+        }, 250);
+    });
 
     // patch CustomEvent to allow constructor creation (IE/Chrome)
     if (typeof window.CustomEvent !== 'function') {
@@ -28,55 +74,4 @@
         window.CustomEvent.prototype = window.Event.prototype;
     }
 
-    /**
-     * Add <html data-orientation="portrait|landscape" is the device has a notch
-     * @returns {void}
-     */
-    function updateOrientationAttribute() {
-        root.setAttribute('data-orientation', (window.innerHeight > window.innerWidth) ? 'portrait' : 'landscape');
-    }
-
-    // only execute this once the document has loaded (this might need to change!)
-    window.addEventListener('load', function() {
-
-        // get the <html> element
-        root = document.documentElement;
-
-        // add CSS variables so we can read them back
-        root.style.setProperty('--notch-top', 'env(safe-area-inset-top)');
-        root.style.setProperty('--notch-right', 'env(safe-area-inset-right)');
-        root.style.setProperty('--notch-bottom', 'env(safe-area-inset-bottom)');
-        root.style.setProperty('--notch-left', 'env(safe-area-inset-left)');
-
-        // get runtime styles
-        var style = window.getComputedStyle(root);
-
-        // read env values back and check if we have any values
-        var hasNotch = [
-            parseInt(style.getPropertyValue('--notch-top') || '-1', 10),
-            parseInt(style.getPropertyValue('--notch-right') || '-1', 10),
-            parseInt(style.getPropertyValue('--notch-bottom') || '-1', 10),
-            parseInt(style.getPropertyValue('--notch-left') || '-1', 10)
-        ]
-        .some(function(val) {
-            return val > 0;
-        });
-
-        // only if we have a notch
-        if (hasNotch) {
-
-            // set <html data-notch="true"> to allow CSS to tweak the display
-            root.setAttribute('data-notch', 'true');
-
-            // fire global notch-detected event to let the UI know that happened
-            window.dispatchEvent(new CustomEvent('notch-detected', { bubbles: true, cancelable: true }));
-
-            // listen for resize events so we can update data-orientation
-            window.addEventListener('resize', updateOrientationAttribute);
-
-            // ensure we have a data-orientation on load
-            updateOrientationAttribute();
-        }
-    });
-
-}(this, document));
+})(this, document);
